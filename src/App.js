5971c38c3896f3087dc72f9bb91b25f1767922d8
Olegopolonets <opolonetso@gmail.com>
2021-08-16 17:31:18 +0300

    added fuctions remove elements and completed task

--- src/App.js
+++ src/App.js
@@ -16,9 +16,13 @@ function App() {
     }
   };
 
-  const removeTask = (id) => {};
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
 
-  const handleToggle = (id) => {};
+  const handleToggle = (id) => {
+    setTodos([...todos.map((todo) => (todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }))]);
+  };
 
   return (
     <div className="App">
